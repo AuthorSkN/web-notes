c7a8a12c88ab0923e8ec473250cefc27b0375f15
AuthorSkN <author.skn@gmail.com>
2018-12-29 19:25:28 +0400

    mini change

--- webnotes-web/src/main/webapp/js/com/webnotes/models/note_content_model.js
+++ webnotes-web/src/main/webapp/js/com/webnotes/models/note_content_model.js
@@ -1,5 +1,20 @@
 'use strict';
 
 class NoteContentModel {
-    
+
+    constructor() {
+        this.noteKey = 0;
+        this.name = "";
+        this.text = "";
+        this.actions = {};
+    }
+
+    setNoteContent(data) {
+
+    }
+
+    checkAction(actionKey) {
+        this.actions[actionKey].complete = !this.actions[actionKey].complete;
+    }
+
 }
